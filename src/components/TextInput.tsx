dfb9ebf9c5d803fe4367a3aa7ffa3b12e304ac5c
Daniel Carreon <daniel.carreong@gmail.com>
2025-02-12 16:18:21 -0600

    Fixes in styles for small screens

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -6,6 +6,6 @@ export const TextInput = ({type, name, placeholder, value}: {
 }) => {
     return (
         <input type={type} name={name} placeholder={placeholder} value={value}
-               className="input w-83 sm:w-90 rounded-full my-2 py-6 bg-white placeholder:text-[#BFBFBF] text-black text-lg "/>
+               className="input w-83 sm:w-83 rounded-full my-2 py-6 bg-white placeholder:text-[#BFBFBF] text-black text-lg "/>
     )
 }
