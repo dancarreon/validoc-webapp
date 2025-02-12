9f97061a0c24be4e0815a9ff1e126a4cd3fe6153
Daniel Carreon <daniel.carreong@gmail.com>
2025-02-12 16:21:51 -0600

    Fixes in logo alignment

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import logo from '../assets/capitoil_logo.png'
 
 export const Logo = () => {
     return (
-        <div className="bg-black rounded-t-xl mb-5 sm:mb-5 pt-10 pb-5">
+        <div className="bg-black rounded-t-xl mb-5 sm:mb-5 pt-10 pb-5 pl-3.5">
             <img src={logo} alt="Capitoil" className='rounded-t-xl'/>
         </div>
     )
