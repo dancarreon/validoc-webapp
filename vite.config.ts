88926fee374c6f35b1542ac9e734226cbb3fbe7b
Daniel Carreon <daniel.carreong@gmail.com>
2025-02-26 15:46:27 -0600

    - Changes for CORS

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [react(), tailwindcss(),],
     server: {
-        cors: false,
+        proxy: {
+            '/': 'https://capitoil-api.vercel.app'
+        }
     }
 })
