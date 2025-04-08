4f93b5ed49ed891178d96539ceb187dc9b5e1986
dancarreon <daniel.carreong@gmail.com>
2025-04-08 17:24:28 -0600

    - Fixes in Claves.tsx

--- src/pages/admin/claves/Claves.tsx
+++ src/pages/admin/claves/Claves.tsx
@@ -1,5 +1,5 @@
 import {SubHeader, SubHeaderProps} from "../../../components/SubHeader.tsx";
-import {useEffect, useState} from "react";
+import {MouseEvent, useEffect, useState} from "react";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {getAllClaves, getTotalClaves} from "../../../api/claves-api.ts";
 import {getOrderAndSort} from "../../utils/utils.ts";
@@ -14,111 +14,111 @@ import {Pagination} from "../../../components/Pagination.tsx";
 import {Spinner} from "../../../components/Spinner.tsx";
 
 const subheaderProps: SubHeaderProps[] = [
-    {title: 'Name', dbProperty: 'name', sort: 'asc'},
+  {title: 'Name', dbProperty: 'name', sort: 'asc'},
 ];
 
 export const Claves = () => {
-    const [claveList, setClaveList] = useState<ClaveType[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [totalClaves, setTotalClaves] = useState(0);
+  const [claveList, setClaveList] = useState<ClaveType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalClaves, setTotalClaves] = useState(0);
 
-    const {register, handleSubmit, getValues} = useForm<SearchType>();
+  const {register, handleSubmit, getValues} = useForm<SearchType>();
 
-    async function doSearch(pageToGo: number, searchString: string): Promise<void> {
-        setIsLoading(true);
-        const clavesFound = await getAllClaves(pageToGo, PAGE_SIZE, searchString);
-        if (clavesFound) {
-            setClaveList(clavesFound);
-        }
-
-        const totalClavesFound = await getTotalClaves(searchString);
-        if (totalClavesFound) {
-            setTotalClaves(totalClavesFound);
-            setCurrentPage(pageToGo);
-        }
-        setIsLoading(false);
+  async function doSearch(pageToGo: number, searchString: string): Promise<void> {
+    setIsLoading(true);
+    const clavesFound = await getAllClaves(pageToGo, PAGE_SIZE, searchString);
+    if (clavesFound) {
+      setClaveList(clavesFound);
     }
 
-    async function fetchClavesCount(): Promise<void> {
-        const totalClaves = await getTotalClaves();
-        if (totalClaves) {
-            setTotalClaves(totalClaves);
-        }
+    const totalClavesFound = await getTotalClaves(searchString);
+    if (totalClavesFound) {
+      setTotalClaves(totalClavesFound);
+      setCurrentPage(pageToGo);
+    }
+    setIsLoading(false);
+  }
+
+  async function fetchClavesCount(): Promise<void> {
+    const totalClaves = await getTotalClaves();
+    if (totalClaves) {
+      setTotalClaves(totalClaves);
+    }
+  }
+
+  async function fetchAllClaves(pageToGo: number) {
+    setIsLoading(true);
+
+    const orderAndSort = getOrderAndSort(subheaderProps);
+
+    const allClaves = await getAllClaves(pageToGo, PAGE_SIZE, getValues('search'), orderAndSort);
+    if (allClaves) {
+      setClaveList(allClaves);
+      setCurrentPage(pageToGo);
     }
 
-    async function fetchAllClaves(pageToGo: number) {
-        setIsLoading(true);
+    await fetchClavesCount();
+    setIsLoading(false);
+  }
 
-        const orderAndSort = getOrderAndSort(subheaderProps);
+  const onSubmit: SubmitHandler<SearchType> = async (searchData: SearchType) => {
+    await doSearch(0, searchData.search);
+  };
 
-        const allClaves = await getAllClaves(pageToGo, PAGE_SIZE, getValues('search'), orderAndSort);
-        if (allClaves) {
-            setClaveList(allClaves);
-            setCurrentPage(pageToGo);
+  const handlePagination = async (e: MouseEvent<HTMLInputElement>) => {
+    const pageToGo: number = Number(e.currentTarget.value);
+    if (currentPage !== pageToGo) {
+      if (getValues('search') === '') {
+        await fetchAllClaves(pageToGo);
+      } else {
+        await doSearch(pageToGo, getValues('search'));
+      }
+    }
+  };
+
+  const handleSort = async (e: MouseEvent<HTMLButtonElement>) => {
+    setIsLoading(true);
+
+    const orderBy = e.currentTarget.name;
+    const orderAndSort = getOrderAndSort(subheaderProps);
+
+    const allClaves = await getAllClaves(Number(currentPage), PAGE_SIZE, getValues('search'), orderAndSort);
+    if (allClaves) {
+      setClaveList(allClaves);
+      subheaderProps.forEach((prop) => {
+        if (prop.dbProperty === orderBy) {
+          prop.sort = prop.sort === 'asc' ? 'desc' : 'asc';
         }
-
-        await fetchClavesCount();
-        setIsLoading(false);
+      });
     }
 
-    const onSubmit: SubmitHandler<SearchType> = async (searchData: SearchType) => {
-        await doSearch(0, searchData.search);
-    };
+    await fetchClavesCount();
+    setIsLoading(false);
+  };
 
-    const handlePagination = async (e: MouseEvent<HTMLInputElement>) => {
-        const pageToGo: number = Number(e.currentTarget.value);
-        if (currentPage !== pageToGo) {
-            if (getValues('search') === '') {
-                await fetchAllClaves(pageToGo);
-            } else {
-                await doSearch(pageToGo, getValues('search'));
-            }
-        }
-    };
+  useEffect(() => {
+    if (claveList.length === 0) {
+      fetchAllClaves(0).catch(console.error);
+    }
+  }, []);
 
-    const handleSort = async (e: MouseEvent<HTMLButtonElement>) => {
-        setIsLoading(true);
-
-        const orderBy = e.currentTarget.name;
-        const orderAndSort = getOrderAndSort(subheaderProps);
-
-        const allClaves = await getAllClaves(Number(currentPage), PAGE_SIZE, getValues('search'), orderAndSort);
-        if (allClaves) {
-            setClaveList(allClaves);
-            subheaderProps.forEach((prop) => {
-                if (prop.dbProperty === orderBy) {
-                    prop.sort = prop.sort === 'asc' ? 'desc' : 'asc';
-                }
-            });
-        }
-
-        await fetchClavesCount();
-        setIsLoading(false);
-    };
-
-    useEffect(() => {
-        if (claveList.length === 0) {
-            fetchAllClaves(0).catch(console.error);
-        }
-    }, []);
-
-    return (
-        <div className='h-[100%] content-center'>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Search {...register('search')} />
-            </form>
-            <Container styles='my-3'>
-                <Header title='Claves'/>
-                <SubHeader props={subheaderProps} onClick={(event) => handleSort(event)}/>
-                {isLoading ? <Spinner/> : <List isUser={false} elements={{model: 'clave', elements: claveList}}/>}
-            </Container>
-            <Pagination
-                currentPage={currentPage}
-                pageSize={PAGE_SIZE}
-                totalRecords={totalClaves}
-                onClick={(event) => handlePagination(event)}
-            />
-        </div>
-    );
+  return (
+    <div className='h-[100%] content-center'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Search {...register('search')} />
+      </form>
+      <Container styles='my-3'>
+        <Header title='Claves'/>
+        <SubHeader props={subheaderProps} onClick={(event) => handleSort(event)}/>
+        {isLoading ? <Spinner/> : <List isUser={false} elements={{model: 'clave', elements: claveList}}/>}
+      </Container>
+      <Pagination
+        currentPage={currentPage}
+        pageSize={PAGE_SIZE}
+        totalRecords={totalClaves}
+        onClick={(event) => handlePagination(event)}
+      />
+    </div>
+  );
 };
